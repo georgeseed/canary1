85228355772a3ab0e86b39f88af7aa3f30132b97
George Seed <50909308+georgeseed@users.noreply.github.com>
2021-06-23 16:42:42 +0100

    Update cavai_chat.js

--- cavai_chat.js
+++ cavai_chat.js
@@ -20,5 +20,7 @@
         }
     })();
 
-const openBubble = new MouseEvent('click');
-document.querySelector('.adbotic-chatbot-bubble-mob').dispatchEvent(openBubble);
+document.querySelector('#myBtn').addEventListener('click', function() {
+		const openBubble = new MouseEvent('click');
+		document.querySelector('.adbotic-chatbot-bubble-mob').dispatchEvent(openBubble);
+});
