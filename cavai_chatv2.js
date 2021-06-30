cff00e981984c09d08cfbdc0e2632bcaff9eb2c6
George Seed <50909308+georgeseed@users.noreply.github.com>
2021-06-30 11:56:12 +0100

    Create cavai_chatv2.js

---
+++ cavai_chatv2.js
@@ -0,0 +1,35 @@
+//the cavai script should be included in any creative that needs conversational
+    (function() {
+        function async_load(target){
+            var s = target.document.createElement('script');
+            s.type = 'text/javascript';
+            s.id = 'cavai-script';
+            var theUrl = 'https://bannerboyz.cavai.com/js/converses/widgets/converse-widget.089cd4c67af73e8fdf103c8b4fe59c17.min.js?bust=' + Date.now();
+            s.src = theUrl;
+            s.async = true;
+           
+            s.dataset.clickMacro = 'MACRO_PLACEHOLDER';
+            s.dataset.bubbleAppearance = 'custom'
+
+            target.document.body.appendChild(s);
+        }
+        try {
+          async_load(window.top)
+        } catch (error) {
+          async_load(window)
+        }
+    })();
+
+window.onload = function() {
+	document.querySelector('.ayl_v_ckr_b').addEventListener('click', function() {
+			const openBubble = new MouseEvent('click');
+			document.querySelector('.adbotic-chatbot-bubble-mob').dispatchEvent(openBubble);
+	});
+};
+
+window.onload = function() {
+	document.querySelector('.ayl_v_ckr_b').addEventListener('touchstart', function() {
+			const openBubble = new MouseEvent('click');
+			document.querySelector('.adbotic-chatbot-bubble-mob').dispatchEvent(openBubble);
+	});
+};
