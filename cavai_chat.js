44b503653bba19abec3cc7962e892d55e704b1d6
George Seed <50909308+georgeseed@users.noreply.github.com>
2021-07-01 12:20:47 +0100

    Update cavai_chat.js

--- cavai_chat.js
+++ cavai_chat.js
@@ -20,15 +20,14 @@
         }
     })();
 
-window.onload = function() {
-	document.querySelector('.ayl_v_ckr_b').addEventListener('click', function() {
-			const openBubble = new MouseEvent('click');
-			document.querySelector('.adbotic-chatbot-bubble-mob').dispatchEvent(openBubble);
-	});
+   (function() {
+		document.querySelector('.ayl_v_ckr_b').addEventListener('click', function() {
+				const openBubble = new MouseEvent('click');
+				document.querySelector('.adbotic-chatbot-bubble-mob').dispatchEvent(openBubble);
+		});
 
-	document.querySelector('.ayl_v_ckr_b').addEventListener('touchstart', function() {
-			const openBubble = new MouseEvent('click');
-			document.querySelector('.adbotic-chatbot-bubble-mob').dispatchEvent(openBubble);
-	});
-	
-};
+		document.querySelector('.ayl_v_ckr_b').addEventListener('touchstart', function() {
+				const openBubble = new MouseEvent('click');
+				document.querySelector('.adbotic-chatbot-bubble-mob').dispatchEvent(openBubble);
+		});
+	})();
