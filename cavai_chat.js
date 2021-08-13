630026802b3ce74122cacccc1d63222760b2daaf
George Seed <50909308+georgeseed@users.noreply.github.com>
2021-08-13 10:13:09 +0100

    Update cavai_chat.js

--- cavai_chat.js
+++ cavai_chat.js
@@ -1,13 +1,12 @@
-//the cavai script should be included in any creative that needs conversational
-    (function() {
+  (function() {
         function async_load(target){
             var s = target.document.createElement('script');
             s.type = 'text/javascript';
             s.id = 'cavai-script';
-            var theUrl = 'https://bannerboyz.cavai.com/js/converses/widgets/converse-widget.089cd4c67af73e8fdf103c8b4fe59c17.min.js?bust=' + Date.now();
+            var theUrl = 'https://cloud.cavai.com/js/converses/widgets/converse-widget.1627298841620-1627288268379-1626873718672-1621429862720-1617810558896-23fbff718ad66343089e7b871fec1c6e.min.js?bust=' + Date.now();
             s.src = theUrl;
             s.async = true;
-           
+
             s.dataset.clickMacro = 'MACRO_PLACEHOLDER';
             s.dataset.bubbleAppearance = 'custom'
 
@@ -20,14 +19,15 @@
         }
     })();
 
-   (function() {
-		document.querySelector('.ayl_v_ckr_b').addEventListener('click', function() {
-				const openBubble = new MouseEvent('click');
-				document.querySelector('.adbotic-chatbot-bubble-mob').dispatchEvent(openBubble);
-		});
+    window.onload = function() {
+    	document.querySelector('.ayl_v_ckr_b').addEventListener('click', function() {
+    			const openBubble = new MouseEvent('click');
+    			document.querySelector('.adbotic-chatbot-bubble-mob').dispatchEvent(openBubble);
+    	});
 
-		document.querySelector('.ayl_v_ckr_b').addEventListener('touchstart', function() {
-				const openBubble = new MouseEvent('click');
-				document.querySelector('.adbotic-chatbot-bubble-mob').dispatchEvent(openBubble);
-		});
-	})();
+    	document.querySelector('.ayl_v_ckr_b').addEventListener('touchstart', function() {
+    			const openBubble = new MouseEvent('click');
+    			document.querySelector('.adbotic-chatbot-bubble-mob').dispatchEvent(openBubble);
+    	});
+
+    };
