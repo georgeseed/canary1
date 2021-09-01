096fab80ec5384075a8bc1393bc19548122da2ae
George Seed <50909308+georgeseed@users.noreply.github.com>
2021-09-01 11:21:29 +0100

    Update cavai_chat.js

--- cavai_chat.js
+++ cavai_chat.js
@@ -1,10 +1,10 @@
 var target = document.querySelector('.ayl_v_ckr_b');
 var clickElement = document.createElement("div");
-clickElement.style.position = "absolute"
-clickElement.style.left = "0px"
-clickElement.style.top = "0px"
-clickElement.style.width = "100%"
-clickElement.style.height = "100%"
+clickElement.style.position = "absolute";
+clickElement.style.left = "0px";
+clickElement.style.top = "0px";
+clickElement.style.width = "100%";
+clickElement.style.height = "100%";
 target.appendChild(clickElement);
 var clickFunction = function() {
     
@@ -20,22 +20,22 @@ target.addEventListener('click', clickFunction, true);
 target.addEventListener('touchstart', clickFunction, true);
 
 (function() {
-        function async_load(target){
-            var s = target.document.createElement('script');
-            s.type = 'text/javascript';
-            s.id = 'cavai-script';
-            var theUrl = 'https://cloud.cavai.com/js/converses/widgets/converse-widget.1627298841620-1627288268379-1626873718672-1621429862720-1617810558896-23fbff718ad66343089e7b871fec1c6e.min.js?bust=' + Date.now();
-            s.src = theUrl;
-            s.async = true;
+    function async_load(target){
+        var s = target.document.createElement('script');
+        s.type = 'text/javascript';
+        s.id = 'cavai-script';
+        var theUrl = 'https://bannerboyz.cavai.com/js/converses/widgets/converse-widget.089cd4c67af73e8fdf103c8b4fe59c17.min.js?bust=' + Date.now();
+        s.src = theUrl;
+        s.async = true;
+       
+        s.dataset.clickMacro = 'MACRO_PLACEHOLDER';
+        s.dataset.bubbleAppearance = 'custom'
 
-            s.dataset.clickMacro = 'MACRO_PLACEHOLDER';
-            s.dataset.bubbleAppearance = 'custom'
-
-            target.document.body.appendChild(s);
-        }
-        try {
-          async_load(window.top)
-        } catch (error) {
-          async_load(window)
-        }
-    })();
+        target.document.body.appendChild(s);
+    }
+    try {
+      async_load(window.top)
+    } catch (error) {
+      async_load(window)
+    }
+})();
