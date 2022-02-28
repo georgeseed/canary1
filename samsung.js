a37ee973b346374f4e6411768d32eb717d971027
George Seed <50909308+georgeseed@users.noreply.github.com>
2022-02-28 09:58:21 +0000

    Create samsung.js

---
+++ samsung.js
@@ -0,0 +1,53 @@
+setTimeout(function(){ 
+    (function() {
+        function async_load(target){
+            var s = target.document.createElement('script');
+            s.type = 'text/javascript';
+            s.id = 'cavai-script';
+            var theUrl = 'https://cloud.cavai.com/js/converses/widgets/converse-widget.1642773214410-1616524651034-1615298019471-8fc047351dede6fd6182aaa93551bfa6.min.js?bust=' + Date.now();
+            s.src = theUrl;
+            s.async = true;
+            
+            s.dataset.clickMacro = 'MACRO_PLACEHOLDER';
+            s.dataset.bubbleAppearance = 'custom'
+
+            target.document.body.appendChild(s);
+        }
+        try {
+          async_load(window.top)
+        } catch (error) {
+          async_load(window)
+        }
+    })();
+
+var target = top.document.querySelectorAll('.ayl_v_ckr_b')[0];
+var clickElement = document.createElement("div");
+clickElement.style.position = "absolute";
+clickElement.style.left = "0px";
+clickElement.style.top = "0px";
+clickElement.style.width = "100%";
+clickElement.style.height = "100%";
+if(target != null){
+    target.appendChild(clickElement);    
+}else{
+    target = top.document.querySelectorAll('.ayl_v_ckr_b')[0];
+}
+
+var clickFunction = function() {
+    
+    /*cavai code for clicking*/
+    const openBubble = new MouseEvent('click');
+    document.querySelector('.adbotic-chatbot-bubble-mob').dispatchEvent(openBubble);
+
+    event.preventDefault();
+    event.stopPropagation();
+    return false;
+}
+
+
+setTimeout(function(){
+    target.addEventListener('click', clickFunction, true);
+target.addEventListener('touchstart', clickFunction, true);
+},2000);
+
+}, 1000);
