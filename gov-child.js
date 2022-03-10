c79b56fe95a02aac218ca1e2839ce52a3d3fd9c4
George Seed <50909308+georgeseed@users.noreply.github.com>
2022-03-10 10:11:38 +0000

    Update gov-child.js

--- gov-child.js
+++ gov-child.js
@@ -1,4 +1,4 @@
-
+setTimeout(function(){ 
 (function() {
         function async_load(target){
             var s = target.document.createElement('script');
@@ -18,4 +18,36 @@
         } catch (error) {
           async_load(window)
         }
-    })();
+    })();    
+
+        var target = top.document.querySelectorAll('.ayl_v_ckr_b')[0];
+var clickElement = document.createElement("div");
+clickElement.style.position = "absolute";
+clickElement.style.left = "0px";
+clickElement.style.top = "0px";
+clickElement.style.width = "100%";
+clickElement.style.height = "100%";
+if(target != null){
+    target.appendChild(clickElement);    
+}else{
+    target = top.document.querySelectorAll('.ayl_v_ckr_b')[0];
+}
+
+var clickFunction = function() {
+    
+    /*cavai code for clicking*/
+    const openBubble = new MouseEvent('click');
+    document.querySelector('.adbotic-chatbot-bubble-mob').dispatchEvent(openBubble);
+
+    event.preventDefault();
+    event.stopPropagation();
+    return false;
+}
+
+
+setTimeout(function(){
+    target.addEventListener('click', clickFunction, true);
+target.addEventListener('touchstart', clickFunction, true);
+},2000);
+
+}, 1000);
