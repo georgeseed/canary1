0770d49a3b4b5c2a6dc31833bdac4e9c6712490d
George Seed <50909308+georgeseed@users.noreply.github.com>
2022-08-11 13:29:03 +0100

    Update cavai_native_marketing.js

--- cavai_native_marketing.js
+++ cavai_native_marketing.js
@@ -1,10 +1,10 @@
-    // Converse ID 1363-2572-3381-20504 generated 2022-08-03T13:41:34.772Z
+setTimeout(function(){ 
     (function() {
         function async_load(target){
             var s = target.document.createElement('script');
             s.type = 'text/javascript';
             s.id = 'cavai-script';
-            var theUrl = 'https://cloud.cavai.com/js/converses/widgets/converse-widget.1659533964121-1659362885104-1658305666368-1657724053390-1657723473892-1657700843814-f022cc7ed9d4c4ae081176d591895e1b.min.js?bust=' + Date.now();
+            var theUrl = 'https://cloud.cavai.com/js/converses/widgets/converse-widget.1660218221391-1654696712391-1639395570751-1625752699417-1625123653710-1624632654389-1624533128612-cd532e7d2ef0b2f052ecf009006dda4f.min.js?bust=' + Date.now();
             s.src = theUrl;
             s.async = true;
 
@@ -20,44 +20,35 @@
         }
     })();
 
-async function(){
-  await attachHandler()
-  target.addEventListener('click', clickFunction, true);
-  target.addEventListener('touchstart', clickFunction, true);
-}
 
-function attachHandler(){
-int count = 0;
-console.log(count);
-int maxTries = 3;
-    while(true) {
-        try {
-          setTimeout(function(){
-          var target = top.document.querySelectorAll('.ayl_v_ckr_b')[0];
-          var clickElement = document.createElement("div");
-          clickElement.style.position = "absolute";
-          clickElement.style.left = "0px";
-          clickElement.style.top = "0px";
-          clickElement.style.width = "100%";
-          clickElement.style.height = "100%";
-          if(target != null){
-              target.appendChild(clickElement);
-          } else{
-              target = top.document.querySelectorAll('.ayl_v_ckr_b')[0];
-          }},1000);
-        } catch (e) {
-            if (++count == maxTries) throw e;
-        }
+    var target = top.document.querySelectorAll('.ayl_v_ckr_b')[0];
+    var clickElement = document.createElement("div");
+    clickElement.style.position = "absolute";
+    clickElement.style.left = "0px";
+    clickElement.style.top = "0px";
+    clickElement.style.width = "100%";
+    clickElement.style.height = "100%";
+    if(target != null){
+        target.appendChild(clickElement);    
+    }else{
+        target = top.document.querySelectorAll('.ayl_v_ckr_b')[0];
     }
-}
 
-var clickFunction = function() {
+    var clickFunction = function() {
 
-    /*cavai code for clicking*/
-    const openBubble = new MouseEvent('click');
-    document.querySelector('.adbotic-chatbot-bubble-mob').dispatchEvent(openBubble);
+        /*cavai code for clicking*/
+        const openBubble = new MouseEvent('click');
+        document.querySelector('.adbotic-chatbot-bubble-mob').dispatchEvent(openBubble);
 
-    event.preventDefault();
-    event.stopPropagation();
-    return false;
-}
+        event.preventDefault();
+        event.stopPropagation();
+        return false;
+    }
+
+
+    setTimeout(function(){
+        target.addEventListener('click', clickFunction, true);
+    target.addEventListener('touchstart', clickFunction, true);
+    },2000);
+
+    }, 1000);
