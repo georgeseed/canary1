abaafa3ea249440f25c9e54ded5100ec1f34215a
George Seed <50909308+georgeseed@users.noreply.github.com>
2022-09-21 11:10:09 +0100

    Update cavai_native_marketing.js

--- cavai_native_marketing.js
+++ cavai_native_marketing.js
@@ -31,29 +31,36 @@
         return false;
     }
 
-    window.addEventListener('load', 
-      function(event) {
-        console.log('page is fully loaded');
-            try {
-              setTimeout(function(){
-              var target = top.document.querySelectorAll('.ayl_v_ckr_b')[0];
-              var clickElement = document.createElement("div");
-              clickElement.style.position = "absolute";
-              clickElement.style.left = "0px";
-              clickElement.style.top = "0px";
-              clickElement.style.width = "100%";
-              clickElement.style.height = "100%";
-              if(target != null){
-                  target.appendChild(clickElement);
-                  target.addEventListener('click', clickFunction, true);
-                  target.addEventListener('touchstart', clickFunction, true);
-              } else{
-                  target = top.document.querySelectorAll('.ayl_v_ckr_b')[0];
-                  target.addEventListener('click', clickFunction, true);
-                  target.addEventListener('touchstart', clickFunction, true);
-              }},1000);
+function winLoad(callback) {
+  if (document.readyState === 'complete') {
+    callback();
+  } else {
+    window.addEventListener("load", callback);
+  }
+}
 
-              } catch (e) {
-                console.log(e)
-              }
-            })
+winLoad(function() {
+    console.log('page is fully loaded');
+        try {
+          setTimeout(function(){
+          var target = top.document.querySelectorAll('.ayl_v_ckr_b')[0];
+          var clickElement = document.createElement("div");
+          clickElement.style.position = "absolute";
+          clickElement.style.left = "0px";
+          clickElement.style.top = "0px";
+          clickElement.style.width = "100%";
+          clickElement.style.height = "100%";
+          if(target != null){
+              target.appendChild(clickElement);
+              target.addEventListener('click', clickFunction, true);
+              target.addEventListener('touchstart', clickFunction, true);
+          } else{
+              target = top.document.querySelectorAll('.ayl_v_ckr_b')[0];
+              target.addEventListener('click', clickFunction, true);
+              target.addEventListener('touchstart', clickFunction, true);
+          }},1000);
+
+          } catch (e) {
+            console.log(e)
+          }
+        });
