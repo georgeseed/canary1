514c2f5baf79c7726ee8cbe9245d05d65800b968
George Seed <50909308+georgeseed@users.noreply.github.com>
2023-02-14 12:42:31 +0000

    Create my_cavai_native.js

---
+++ my_cavai_native.js
@@ -0,0 +1,53 @@
+(function() {
+  var s   = document.createElement('script');
+  s.src   = 'https://delivery-3.cavai.com/assets/creatives/50924/cle48426o07u20bj4asgzbe2a/built/assets/stub.js?bust='+Date.now();
+  s.async = true;
+  s.setAttribute('data-click-macro', 'MACRO_PLACEHOLDER');
+  s.setAttribute('data-start-flow-trigger-id', ' ');
+  document.head.appendChild(s);
+})();
+
+var clickFunction = function() {
+
+        /*cavai code for clicking*/
+        const openBubble = new MouseEvent('click');
+        document.querySelector('.adbotic-chatbot-bubble-mob').dispatchEvent(openBubble);
+
+        event.preventDefault();
+        event.stopPropagation();
+        return false;
+    }
+
+function winLoad(callback) {
+  if (document.readyState === 'complete') {
+    callback();
+  } else {
+    window.addEventListener("load", callback);
+  }
+}
+
+winLoad(function() {
+    console.log('page is fully loaded');
+        try {
+          setTimeout(function(){
+          var target = top.document.querySelectorAll('.ayl_v_ckr_b')[0];
+          var clickElement = document.createElement("div");
+          clickElement.style.position = "absolute";
+          clickElement.style.left = "0px";
+          clickElement.style.top = "0px";
+          clickElement.style.width = "100%";
+          clickElement.style.height = "100%";
+          if(target != null){
+              target.appendChild(clickElement);
+              target.addEventListener('click', clickFunction, true);
+              target.addEventListener('touchstart', clickFunction, true);
+          } else{
+              target = top.document.querySelectorAll('.ayl_v_ckr_b')[0];
+              target.addEventListener('click', clickFunction, true);
+              target.addEventListener('touchstart', clickFunction, true);
+          }},1000);
+
+          } catch (e) {
+            console.log(`cavai error log: ${e}`)
+          }
+        });
