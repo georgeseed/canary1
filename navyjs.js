979caaaed8b179583fec8bf927e888e2f6b252c9
George Seed <50909308+georgeseed@users.noreply.github.com>
2023-03-15 13:01:07 +0000

    navy.js

---
+++ navyjs.js
@@ -0,0 +1,8 @@
+(function() {
+  var s   = document.createElement('script');
+  s.src   = 'https://delivery-3.cavai.com/assets/creatives/51337/cleill82r0f2e70j4gwso8lz4/built/assets/stub.js?bust='+Date.now();
+  s.async = true;
+  s.setAttribute('data-click-macro', '%c');
+  s.setAttribute('data-clickthrough-url', 'https://ad.doubleclick.net/ddm/trackclk/N6198.3598608CAVAI/B29132513.355250024;dc_trk_aid=546427221;dc_trk_cid=183959446;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;ltd=');
+  document.head.appendChild(s);
+})();
